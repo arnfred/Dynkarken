7376f6713e7c04d55bf788655e80b1a93f3741e0
jonas arnfred <jonas@ifany.org>
2014-05-07 14:24:45 +0000

    Page now more or less works

--- server.py
+++ server.py
@@ -27,8 +27,10 @@ class page :
                 page_json = json.loads(page_fp.read())
             with open("static/pages/%s/page.html" % page_id, 'r') as page_fp :
                 content = page_fp.read().replace("\n\n","\n").replace(">\n<","><")
-            name = page_json["title"]
-            return render.page(name, page_id, content)
+            title = page_json["title"]
+            published = parse_date(page_json["published"])
+            tags = ", ".join(page_json["tags"]) if len(page_json["tags"]) > 0 else ""
+            return render.page(title, published, tags, content, page_id)
         except IOError :
             return "Page '%s' not found" % page_id
 
@@ -36,3 +38,26 @@ class page :
 class index :
     def GET(self):
         return render.main()
+
+def parse_date(date_string) :
+    date_list = date_string[0:10].split("-")
+    year = int(date_list[0])
+    month = {
+        "01" : "January",
+        "02" : "February",
+        "03" : "March",
+        "04" : "April",
+        "05" : "May",
+        "06" : "June",
+        "07" : "July",
+        "08" : "August",
+        "09" : "September",
+        "10" : "October",
+        "11" : "November",
+        "12" : "December"}[date_list[1]]
+    day = int(date_list[2])
+    day_suffix = {
+        "1" : "st",
+        "2" : "nd",
+        "3" : "rd"}.get(str(day)[-1], "th")
+    return "%s %i%s %i" % (month, day, day_suffix, year)
